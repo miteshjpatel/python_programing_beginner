9a2b24d4d97d29614d4a97be6ce42f2e51ec8f29
Mitesh Patel <miteshjpatel@yahoo.com>
2022-10-18 14:06:18 +0000

    Commit

---
+++ totorial2.py
@@ -0,0 +1,24 @@
+## Python Basic Operators and Input
+
+print("Hello, What is yourname?")
+name = input()
+print('Hello', name, '!!')
+
+
+#Python operaton +, -, *, /
+
+num1 = 45
+num2 = 3
+num3 = 4
+num4 = 4
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+#Python **, //, %
+
+print(num1 ** num3)
+print(num1 // num3)
+print(num3 % num3)
