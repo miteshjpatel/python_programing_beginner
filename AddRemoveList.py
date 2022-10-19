40ca849f8580316317019e9ffb704cabbf1d7d5e
Mitesh Patel <miteshjpatel@yahoo.com>
2022-10-19 11:47:23 +0000

    Morning

---
+++ AddRemoveList.py
@@ -0,0 +1,19 @@
+# ADD Remove from ITEM list
+
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange") ## append to insert
+print(thislist)
+
+thislist.insert(1, 'Mango') ## insert to list
+print(thislist)
+
+#Loop through list
+
+for i in range(len(thislist)):
+    print(thislist[i])
+
+thislist.remove('banana')   # Remove banana from list
+print(thislist)
+
+thislist.pop(1)             # Remove the specified index
+print(thislist)
--- tutorial8.py
+++ tutorial8.py
@@ -12,3 +12,17 @@ print(fruits)
 fruits[1] = 'blueberry'
 
 print(fruits)
+
+tuple_name = ('Mitesh', 'Yogita', 'Shivam', 'Dev')
+list_name = ['Mitesh', 'Yogita', 'Shivam', 'Dev']
+
+print(list_name)
+print(tuple_name)
+
+list_name[0] = 'Mitesh Patel'
+
+print(list_name)
+print(list_name[0])
+
+print(tuple_name.count('Mitesh'))
+print(tuple_name[0])
