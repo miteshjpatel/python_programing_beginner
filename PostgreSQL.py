c05f68e4fbe3d0e54f15287b5194bd14099257f2
Mitesh Patel <miteshjpatel@yahoo.com>
2022-10-26 16:18:30 +0000

    MJP Commit Postgresql

--- PostgreSQL.py
+++ PostgreSQL.py
@@ -1,20 +1,21 @@
-from dbm.dumb import _Database
 import psycopg2
+import getpass
 
-
+dbuser = input("Enter db User id: ")
+dbpass = getpass.getpass("Enter db password: ")
 #connect to the db
 con = psycopg2.connect(
             host = "mjUbuntu",
             database = "atadb",
-            user = "ata",
-            password = "SapGss123"
+            user = dbuser,
+            password = dbpass
 )
 
 #cursor
 cur = con.cursor()
 
-cur.execute("insert into test (name, address) values(%s, %s)", ("Yogita", "900 West st, ST Louis, MI"))
-con.commit()  
+# cur.execute("insert into test (name, address) values(%s, %s)", ("Yogita", "900 West st, ST Louis, MI"))
+# con.commit()  
 
 #execute query
 cur.execute("select name, address from test")
